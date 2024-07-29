186d5ad7badd34d78a52ce55bf7e98d3bbb443dc
Ruben Garcia <rgarcr09@gmail.com>
2024-07-29 18:47:42 -0400

    Added lvl 8 kyu challenge FindtheDifferenceinAge

---
+++ 8 kyu/FindthDifferenceinAgeOldYoung.js
@@ -0,0 +1,20 @@
+/**
+ At the annual family gathering, the family likes to find the oldest living family member’s age 
+ and the youngest family member’s age and calculate the difference between them.
+
+ You will be given an array of all the family members' ages, in any order. 
+ The ages will be given in whole numbers, so a baby of 5 months, will have an ascribed ‘age’ of 0.
+ Return a new array (a tuple in Python) with [youngest age, oldest age, difference between the youngest and oldest age].
+ */
+
+//My Solution
+
+function differenceInAges(ages) {
+    //store highest value from array
+    let oldest = Math.max(...ages);
+    //store smallest value from array
+    let youngest = Math.min(...ages);
+    //store the difference 
+    let difference = oldest - youngest;
+    return [youngest, oldest, difference];
+}
