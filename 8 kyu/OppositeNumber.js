c9734d95477bce719726add6cd798187fd3bc675
Ruben Garcia <rgarcr09@gmail.com>
2024-08-08 19:38:21 -0400

    Added lvl 8kyu OppositeNumbers

---
+++ 8 kyu/OppositeNumber.js
@@ -0,0 +1,7 @@
+//Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+//My Solution
+function opposite(number) {
+    return -number;
+}
+
