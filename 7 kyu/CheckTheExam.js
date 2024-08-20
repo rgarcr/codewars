62476d042a94378ac61d4918af233b9dd92c0e5a
Ruben Garcia <rgarcr09@gmail.com>
2024-08-20 07:04:33 -0400

    Added lvl 7kyu CheckTheExam

---
+++ 7 kyu/CheckTheExam.js
@@ -0,0 +1,17 @@
+// The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+// The two arrays are not empty and are the same length. Return the score for this array of answers, 
+// giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, 
+// represented as an empty string (in C the space character is used).
+
+// If the score < 0, return 0
+
+//My solution
+
+function checkExam(array1, array2) {
+    let result = array1.reduce((acc, curr, index) => {
+        acc += (curr === array2[index] ? 4 : (array2[index] ? -1 : 0));
+        return acc;
+    }, 0);
+    return result < 0 ? 0 : result;
+}
