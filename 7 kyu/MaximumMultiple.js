3c5c39302ae0fb1dff1876a8a9b076aa580743c8
Ruben Garcia <rgarcr09@gmail.com>
2024-09-17 18:09:18 -0400

    Added lvl 7kyu Maximum Multiple

---
+++ 7 kyu/MaximumMultiple.js
@@ -0,0 +1,13 @@
+// Given a Divisor and a Bound , Find the largest integer N , Such That ,
+
+// Conditions :
+// N is divisible by divisor
+
+// N is less than or equal to bound
+
+// N is greater than 0.
+
+//My solution
+function maxMultiple(divisor, bound) {
+    return Math.floor(bound / divisor) * divisor;
+}
