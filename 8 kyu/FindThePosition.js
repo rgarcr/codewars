8bd2b96e2f01bc8ddd2f2807ec0d5693c0c602b0
Ruben Garcia <rgarcr09@gmail.com>
2024-12-05 07:07:33 -0500

    Added lvl 8kyu FIndThePosition

---
+++ 8 kyu/FindThePosition.js
@@ -0,0 +1,13 @@
+// When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Output :: "Position of alphabet: 1"
+
+// Note: Only lowercased English letters are tested
+
+//My solution
+
+function position(letter) {
+    return `Position of alphabet: ${letter.charCodeAt() - 96}`
+}
