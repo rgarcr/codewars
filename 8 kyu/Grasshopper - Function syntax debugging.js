f661967b33819939f89ff64d92f73ee4944a30c8
Ruben Garcia <rgarcr09@gmail.com>
2025-01-15 17:06:49 -0500

    Added lvl 8kyu  Grasshopper - Function syntax debugging

---
+++ 8 kyu/Grasshopper - Function syntax debugging.js
@@ -0,0 +1,5 @@
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+//My solution
+
+const main = (verb, noun) => verb + noun;
