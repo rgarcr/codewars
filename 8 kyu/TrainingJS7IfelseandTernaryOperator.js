e75dc6c646373f965b16b3cd4b028187381f0a3f
Ruben Garcia <rgarcr09@gmail.com>
2025-01-16 06:12:08 -0500

    Added lvl 8kyu Training JS #7: if..else and ternary operator

---
+++ 8 kyu/TrainingJS7IfelseandTernaryOperator.js
@@ -0,0 +1,9 @@
+// Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, 
+//function accepts 1 parameter:n, n is the number of hotdogs a customer will buy, different numbers have different prices (refer to the following table), return how much money will the customer spend to buy that number of hotdogs.
+
+
+//My solution 
+
+function saleHotdogs(n) {
+    return n < 5 ? n * 100 : n >= 10 ? n * 90 : n * 95;
+}
