560e9e19db99d5bd9f2bfe09aec453e5ed62596f
Ruben Garcia <rgarcr09@gmail.com>
2025-04-10 12:56:58 -0400

    Added lvl 7kyu Regexp Basics - is it a vowel?

---
+++ 7 kyu/Regexp BasicsIsItAVowel.js
@@ -0,0 +1,7 @@
+// Implement the function which should return true if given object is a vowel (meaning a, e, i, o, u, uppercase or lowercase), and false otherwise.
+
+//My solution
+
+String.prototype.vowel = function () {
+    return /[aeiou]/gi.test(this.toString()) && this.toString().length === 1;
+};
