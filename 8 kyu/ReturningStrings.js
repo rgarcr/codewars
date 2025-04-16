1abf61c3aadb4992860da9de0435baf7d7e6467c
Ruben Garcia <rgarcr09@gmail.com>
2025-04-16 06:38:00 -0400

    Added lvl 8kyu Returning Strings

---
+++ 8 kyu/ReturningStrings.js
@@ -0,0 +1,4 @@
+// Create a function that accepts a parameter representing a name and returns the message: "Hello, <name> how are you doing today?".
+
+//My solution
+const greet = name => `Hello, ${name} how are you doing today?`;
