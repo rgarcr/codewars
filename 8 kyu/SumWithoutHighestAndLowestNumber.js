36a53e6a75f65f468adf00b847df66c45184a65b
Ruben Garcia <rgarcr09@gmail.com>
2026-02-02 18:10:48 -0500

    Added lvl 8kyu Sum without highest and lowest number

---
+++ 8 kyu/SumWithoutHighestAndLowestNumber.js
@@ -0,0 +1,23 @@
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+// Mind the input validation.
+
+// Example
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+// Input validation
+// If an empty value ( null, None, Nothing, nil etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+//My solution
+
+function sumArray(array) {
+    if (!array || array.length <= 1)
+        return 0
+    const max = Math.max(...array)
+    const min = Math.min(...array)
+    const sum = array.reduce((a, c) => a += c)
+
+    return sum - max - min;
+}
