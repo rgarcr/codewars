a2dfffc8a66561a4f9de2e20772df60cbfce305a
Ruben Garcia <rgarcr09@gmail.com>
2026-02-17 14:39:53 -0500

    Added lvl 8kyu Simple multiplication

---
+++ 8 kyu/SimpleMultiplication.js
@@ -0,0 +1,4 @@
+// This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
+
+// My solution
+const simpleMultiplication = num => num % 2 === 0 ? num * 8 : num * 9;
