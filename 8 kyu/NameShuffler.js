1493b53e21df6dd782a168f1fc96dd44ffd099aa
rgarcr <66577998+rgarcr@users.noreply.github.com>
2026-06-10 18:08:15 -0400

    Added lvl 8kyu NameShuffler

---
+++ 8 kyu/NameShuffler.js
@@ -0,0 +1,6 @@
+// Write a function that returns a string in which firstname is swapped with last name.
+
+//my solution
+function nameShuffler(str) {
+    return str.split(' ').reverse().join(" ");
+}
