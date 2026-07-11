91f6232dfb9cbb0e1eea94323aa832887d29d798
rgarcr <66577998+rgarcr@users.noreply.github.com>
2026-07-10 23:18:21 -0400

    Added lvl 8kyu ConvertToBinary

---
+++ 8 kyu/ConvertToBinary.js
@@ -0,0 +1,6 @@
+// Given a non-negative integer b, write a function which returns an integer d such that the binary representation of b is the same as the decimal representation of d.
+
+//My solution
+function toBinary(n) {
+    return +n.toString(2);
+}
