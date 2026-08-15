8ec039dad8128d4e43da55447eb53bd7086b5db2
rgarcr <66577998+rgarcr@users.noreply.github.com>
2026-08-15 07:58:15 -0400

    Added lvl 8kyu ReversedStrings_SQL

---
+++ 8 kyu/ReversedStrings_SQL.sql
@@ -0,0 +1,9 @@
+// Complete the solution so that it reverses the string passed into it.
+
+// 'world'  =>  'dlrow'
+// 'word'   =>  'drow'
+// Strings
+
+//My solution
+SELECT str, REVERSE(str) as res FROM solution
+  order by str 
