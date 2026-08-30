506b29da11e0b4afd395139c9b3ab8f9214417a8
rgarcr <66577998+rgarcr@users.noreply.github.com>
2026-08-30 14:53:32 -0400

    Added lvl 8kyu RemoveExclamationMarks

---
+++ 8 kyu/RemoveExclamationMarks.sql
@@ -0,0 +1,8 @@
+-- Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+//My solution
+-- # write your SQL statement here: you are given a table 'removeexclamationmarks' with column 's', return a table with column 's' and your result in a column named 'res'.
+SELECT
+  s,
+  REPLACE(s, '!', '') AS res
+FROM removeexclamationmarks
