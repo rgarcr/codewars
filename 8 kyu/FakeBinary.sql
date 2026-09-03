ced34898d70d37da5a506fcb72c94bc4415817b1
rgarcr <66577998+rgarcr@users.noreply.github.com>
2026-09-03 10:26:39 -0400

    Added lvl 8kyu FakeBinary

---
+++ 8 kyu/FakeBinary.sql
@@ -0,0 +1,8 @@
+-- Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+-- Note: input will never be an empty string
+
+//My solution
+-- # write your SQL statement here: you are given a table 'fakebin' with column 'x', return a table with column 'x' and your result in a column named 'res'.
+SELECT x,REGEXP_REPLACE(REGEXP_REPLACE(x,'[0-4]','0','g'),'[5-9]','1','g') AS res
+FROM fakebin
