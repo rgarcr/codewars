0b00845baab39c9127c62b245c1583b643c03610
rgarcr <66577998+rgarcr@users.noreply.github.com>
2026-09-07 09:04:30 -0400

    Added lvl 7kyu EasySQLCountingAndGrouping

---
+++ 7 kyu/EasySQLCountingAndGrouping.sql
@@ -0,0 +1,21 @@
+-- Given a demographics table in the following format:
+
+-- ** demographics table schema **
+
+-- id
+-- name
+-- birthday
+-- race
+-- you need to return a table that shows a count of each race represented, ordered by the count in descending order as:
+
+-- ** output table schema **
+
+-- race
+-- count
+
+--My solution
+
+SELECT race, COUNT(race) AS count
+FROM demographics
+GROUP BY race
+ORDER BY count DESC;
