b7d22c49fd4c6c030f1f6cff1747e06a7bdaa1ac
rgarcr <66577998+rgarcr@users.noreply.github.com>
2026-09-16 06:58:47 -0400

    Added lvl 8kyu SQLBasicsMod

---
+++ 8 kyu/SQLBasicsMod.sql
@@ -0,0 +1,11 @@
+-- Given the following table 'decimals':
+
+-- ** decimals table schema **
+
+-- id
+-- number1
+-- number2
+-- Return a table with one column (mod) which is the output of number1 modulus number2.
+
+--my solution
+SELECT number1 % number2 AS mod FROM decimals
